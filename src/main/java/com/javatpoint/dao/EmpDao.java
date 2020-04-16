a6427a17e5e733e28720cca2c37ec59e9dcc4143
esaaray <sanghamitra.ray@ericsson.com>
2020-04-16 15:22:54 +0530

    emplyee increased

--- src/main/java/com/javatpoint/dao/EmpDao.java
+++ src/main/java/com/javatpoint/dao/EmpDao.java
@@ -18,7 +18,7 @@ public class EmpDao {
 public List<Emp> getEmployees(){  
 	Emp employee1 = new Emp();
 	Emp employee2 = new Emp();
-	//Emp employee3 = new Emp();	
+	Emp employee3 = new Emp();	
 	employee1.setId(1);
 	employee1.setName("John");
 	employee1.setDesignation("architect");
@@ -29,15 +29,17 @@ public List<Emp> getEmployees(){
 	employee2.setDesignation("engineer");
 	employee2.setSalary(90000);	
 		
-		/*
-		 * employee3.setId(3); employee3.setName("Tom");
-		 * employee3.setDesignation("doctor"); employee3.setSalary(20000);
-		 */
+		
+	employee3.setId(3); 
+	employee3.setName("Tom");
+	employee3.setDesignation("doctor"); 
+	employee3.setSalary(20000);
+		 
 		 	
 	List<Emp> empList = new ArrayList<Emp>();
 	empList.add(employee1);
 	empList.add(employee2);
-	//empList.add(employee3);	
+	empList.add(employee3);	
     return empList;
 }  
 
